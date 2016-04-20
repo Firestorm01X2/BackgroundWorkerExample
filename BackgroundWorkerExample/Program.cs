1dbf4643c0823c961e0062346a31f0fa81e5ce78
Firestorm01X2 <firestorm01x2@gmail.com>
2016-04-20 09:03:45 +0300

    Добавил важный комментарий

--- BackgroundWorkerExample/Program.cs
+++ BackgroundWorkerExample/Program.cs
@@ -14,6 +14,7 @@ namespace BackgroundWorkerExample
         [STAThread]
         static void Main()
         {
+            //Важный комментарий
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
